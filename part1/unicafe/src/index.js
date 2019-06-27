bca2003b8ae3337a9ab8a673bb3233ce903fa907
Daksh <dakshjain48@gmail.com>
2019-06-27 18:14:11 +0530

    updated indentation

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,63 +2,63 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Statistic = (props) => {
-	if (props.val === undefined)
-		return (
-			<>
-				<td>{props.text}</td>
-			</>
-		)
+  if (props.val === undefined)
+    return (
+      <>
+        <td>{props.text}</td>
+      </>
+    )
 
-	else if (props.text === 'positive')
-		return (
-			<>
-				<td>{props.text}</td> 
-				<td>{props.val} %</td>
-			</>
-		)
+  else if (props.text === 'positive')
+    return (
+      <>
+        <td>{props.text}</td> 
+        <td>{props.val} %</td>
+      </>
+    )
 
-	return (
-		<>
-			<td>{props.text}</td>
-			<td>{props.val}</td>
-		</>
-	)
+  return (
+    <>
+      <td>{props.text}</td>
+      <td>{props.val}</td>
+    </>
+  )
 }
 
 const Statistics = (props) => {
-	const [good, neutral, bad] = props.val
-	const all = good + neutral + bad
-	const average = () => ((1*good) + (-1*bad))/all
+  const [good, neutral, bad] = props.val
+  const all = good + neutral + bad
+  const average = () => ((1*good) + (-1*bad))/all
 
-	if (all === 0)
-		return (
-			<table>
-				<tbody>
-					<tr><Statistic text='No feedback given' /></tr>
-				</tbody>
-			</table>
-		)
+  if (all === 0)
+    return (
+      <table>
+        <tbody>
+          <tr><Statistic text='No feedback given' /></tr>
+        </tbody>
+      </table>
+    )
 
-	return (
-		<table>
-			<tbody>
-				<tr><Statistic text='good' val={good} /></tr>
-				<tr><Statistic text='neutral' val={neutral} /></tr>
-				<tr><Statistic text='bad' val={bad} /></tr>
-				<tr><Statistic text='all' val={all} /></tr>
-				<tr><Statistic text='average' val={average()} /></tr>
-				<tr><Statistic text='positive' val={good*100/all} /></tr>
-			</tbody>
-		</table>
-	)
+  return (
+    <table>
+      <tbody>
+        <tr><Statistic text='good' val={good} /></tr>
+        <tr><Statistic text='neutral' val={neutral} /></tr>
+        <tr><Statistic text='bad' val={bad} /></tr>
+        <tr><Statistic text='all' val={all} /></tr>
+        <tr><Statistic text='average' val={average()} /></tr>
+        <tr><Statistic text='positive' val={good*100/all} /></tr>
+      </tbody>
+    </table>
+  )
 }
 
 const Button = (props) => {
-	return (
-		<>
-			<button onClick={props.func}>{props.text}</button>
-		</>
-	)
+  return (
+    <>
+      <button onClick={props.func}>{props.text}</button>
+    </>
+  )
 }
 
 const App = () => {
@@ -66,27 +66,27 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  
+  const updateCount = (str, count) => {
+    if (str === 'good') {
+      return () => setGood(count + 1)
+    }
+    else if (str === 'neutral') {
+      return () => setNeutral(count + 1)
+    }
+    else {
+      return () => setBad(count + 1)
+    }
+  }
 
-	const updateCount = (str, count) => {
-		if (str === 'good') {
-			return () => setGood(count + 1)
-		}
-		else if (str === 'neutral') {
-			return () => setNeutral(count + 1)
-		}
-		else {
-			return () => setBad(count + 1)
-		}
-	}
-
-	return (
+  return (
     <div>
-			<h1>give feedback</h1>
-			<Button func={updateCount('good', good)} text={'good'} />
-			<Button func={updateCount('neutral', neutral)} text={'neutral'} />
-			<Button func={updateCount('bad', bad)} text={'bad'} />
-			<h1>statistics</h1>
-			<Statistics val={[good, neutral, bad]} />
+      <h1>give feedback</h1>
+      <Button func={updateCount('good', good)} text={'good'} />
+      <Button func={updateCount('neutral', neutral)} text={'neutral'} />
+      <Button func={updateCount('bad', bad)} text={'bad'} />
+      <h1>statistics</h1>
+      <Statistics val={[good, neutral, bad]} />
     </div>
   )
 }
