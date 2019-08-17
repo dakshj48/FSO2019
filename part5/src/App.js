f6d4801f9aa76507b06a86b5b7d5f70c89115076
Daksh <dakshjain48@gmail.com>
2019-08-17 13:15:31 +0530

    5.3 complete

--- part5/src/App.js
+++ part5/src/App.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Login from './components/Login'
 import blogService from './services/blogs'
 import loginService from './services/login'
-import Login from './components/Login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [newBlog, setNewBlog] = useState('')
+ // const [newBlog, setNewBlog] = useState('')
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [title, setTitle] = useState("")
+  const [author, setAuthor] = useState("")
+  const [url, setUrl] = useState("")
 
   useEffect(() => {
     blogService
@@ -28,6 +31,7 @@ const App = () => {
       window.localStorage.setItem(
         'loggedBlogappUser', JSON.stringify(user)
       )
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -35,6 +39,26 @@ const App = () => {
     } catch (error) {
       setErrorMessage('Wrong Credentials')
       console.log('Wrong Credentials')
+      console.log(error)
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleNewBlog = async (event) => {
+    event.preventDefault()
+    try {
+      const newBlog = await blogService.create({ "title": title, "author": author, "url": url })
+      setBlogs(blogs.concat(newBlog))
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+    }
+    catch (error) {
+      setErrorMessage('Failed to save the blog')
+      console.log('Failed to save the blog')
+      console.log(error)
       setTimeout(() => {
         setErrorMessage(null)
       }, 5000)
@@ -50,10 +74,14 @@ const App = () => {
     <div>
       {user === null && 
         <Login handleLogin={handleLogin} username={username} password={password} 
-        setUsername={setUsername} setPassword={setPassword}
-      />}
+          setUsername={setUsername} setPassword={setPassword}
+        />
+      }
       {user !== null &&
-        <Blog blogs={blogs} user={user} handleLogout={handleLogout} />
+        <Blog blogs={blogs} user={user} title={title} author={author} url={url}
+          setTitle={setTitle} setAuthor={setAuthor} setUrl={setUrl} 
+          handleLogout={handleLogout} handleNewBlog={handleNewBlog}
+        />
       }
     </div>
   )
--- part5/src/components/Login.js
+++ part5/src/components/Login.js
@@ -2,25 +2,25 @@ import React from 'react'
 
 const Login = ({ handleLogin, username, password, setUsername, setPassword }) => (
   <div>
-      <h1>
-        log in to application
-      </h1>
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-            <input type='text' value={username} name='Username'
-              onChange={({ target }) => setUsername(target.value)}
-            />
-        </div>
-        <div>
-          password
-            <input type='password' value={password} name='Password'
-              onChange={({ target }) => setPassword(target.value)}
-            />
-        </div>
-        <button type='submit'>login</button>
-      </form>
-    </div>
+    <h1>
+      log in to application
+    </h1>
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input type='text' value={username} name='Username'
+            onChange={({ target }) => setUsername(target.value)}
+          />
+      </div>
+      <div>
+        password
+          <input type='password' value={password} name='Password'
+            onChange={({ target }) => setPassword(target.value)}
+          />
+      </div>
+      <button type='submit'>login</button>
+    </form>
+  </div>
 )
 
 export default Login
